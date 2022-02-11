82d49d84a74677cdfa6ceec9bc31a1d7464f2a49
ESThing64 <89550738+ESThing64@users.noreply.github.com>
2022-02-10 20:13:40 -0600

    Setup on the skeleton template, the card and dummy data, setup firebaseauth

---
+++ src/config.js
@@ -0,0 +1,31 @@
+const config = {
+    // basename: only at build time to set, and Don't add '/' at end off BASENAME for breadcrumbs, also Don't put only '/' use blank('') instead,
+    // like '/berry-material-react/react/default'
+    basename: '',
+    defaultPath: '/sample-page',
+    fontFamily: `'Roboto', sans-serif`,
+    borderRadius: 12,
+    outlinedFilled: true,
+    theme: 'light', // light, dark
+    presetColor: 'default', // default, theme1, theme2, theme3, theme4, theme5, theme6
+    i18n: 'en', // 'en' - English, 'fr' - French, 'ro' - Romanian, 'zh' - Chinese
+    rtlLayout: false,
+    jwt: {
+        secret: 'SECRET-KEY',
+        timeout: '1 days'
+    },
+    firebase: {
+        apiKey: 'AIzaSyAsQVIKFa8zFni7YvG54YcsmqmHdDwCwL4',
+        authDomain: 'endeavor88.firebaseapp.com',
+        projectId: 'endeavor88',
+        storageBucket: 'endeavor88.appspot.com',
+        messagingSenderId: '887984495883',
+        appId: '1:887984495883:web:581c52c6000a7eaa8c960e'
+    },
+    auth0: {
+        client_id: '7T4IlWis4DKHSbG8JAye4Ipk0rvXkH9V',
+        domain: 'dev-w0-vxep3.us.auth0.com'
+    }
+};
+
+export default config;
